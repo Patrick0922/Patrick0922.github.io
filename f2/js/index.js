80b3cdf8fcf0b91195004132f1e374aa2b5570ef
Patrick0922 <jorsini@sjnma.org>
2019-01-08 08:43:22 -0600

    final

---
+++ f2/js/index.js
@@ -0,0 +1,31 @@
+
+function annoy(){
+var fname = prompt("tell me your name");
+
+document.getElementById("annoy").innerHTML = "I am annoyed";
+while(true){
+alert(fname + " are we there yet?");
+}
+
+}
+
+function mark_present()
+{
+    var cadet_name = $('#cadet_name').val();
+    alert(cadet_name+ " is here!");
+    $('#present_cadets').append("<ul id = 'cadets_list'></ul>");
+    $('#cadets_list').append("<li>"+cadet_name+"</li>")
+    
+}
+
+$(document).ready(function() {
+    var flag = false;
+    setInterval(function() {
+        flag = !flag;
+        $("body").css("background", flag ? "red" : "green");
+        $("body").css("background", flag ? "red" : "green");
+    }, 1000);
+
+});
+
+    
